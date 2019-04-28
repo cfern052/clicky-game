f9f1012b7143e7619a215f0e1f30f8c61b36ffb2
cfern052 <cfern052@gmail.com>
2019-04-28 11:39:21 -0400

    added title and wrapper components. added image json

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from "./components/Title";
+import imageCard from "./components/imageCard"
 
 function App() {
   return (
